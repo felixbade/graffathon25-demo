140cd5194b2f1ae25efd1c5625ed8ea0f4786e04
Felix Bade <flix.bade@gmail.com>
2025-06-07 22:53:18 +0300

    new params

--- theatre-state.js
+++ theatre-state.js
@@ -4,7 +4,23 @@ const projectState = {
             "staticOverrides": {
                 "byObject": {
                     "Heading 1": {
-                        "opacity": 0.5886075949367084
+                        "opacity": 0.5886075949367084,
+                        "boxZ": 2,
+                        "boxY": 6,
+                        "boxX": 29,
+                        "moveY": -4,
+                        "rotateX": -0.0126582278481013,
+                        "rotateY": 0.4810126582278478,
+                        "rotateA": 0,
+                        "shadeR": 0.4050632911392404,
+                        "shadeB": 0.189873417721519,
+                        "shadeG": 0.30379746835443044,
+                        "moveX": 12,
+                        "moveZ": 6,
+                        "mixR": -0.24050632911392394,
+                        "mixB": 0.22784810126582278,
+                        "mixG": 0.16455696202531647,
+                        "rotateZ": 0.012658227848101092
                     }
                 }
             },
@@ -56,10 +72,76 @@ const projectState = {
                                         "value": 0
                                     }
                                 ]
+                            },
+                            "LdTv2JyGT_": {
+                                "type": "BasicKeyframedTrack",
+                                "__debugName": "Heading 1:[\"boxX\"]",
+                                "keyframes": [
+                                    {
+                                        "id": "R9rSwUZNwJ",
+                                        "position": 0,
+                                        "connectedRight": true,
+                                        "handles": [
+                                            0.5,
+                                            1,
+                                            0.5,
+                                            0
+                                        ],
+                                        "type": "bezier",
+                                        "value": 51
+                                    },
+                                    {
+                                        "id": "R6seMcQirK",
+                                        "position": 3.4,
+                                        "connectedRight": true,
+                                        "handles": [
+                                            0.5,
+                                            1,
+                                            0.5,
+                                            0
+                                        ],
+                                        "type": "bezier",
+                                        "value": 11
+                                    },
+                                    {
+                                        "id": "sqDUXisXNW",
+                                        "position": 7.767,
+                                        "connectedRight": true,
+                                        "handles": [
+                                            0.5,
+                                            1,
+                                            0.5,
+                                            0
+                                        ],
+                                        "type": "bezier",
+                                        "value": 2
+                                    }
+                                ]
+                            },
+                            "9YTvAO-ziA": {
+                                "type": "BasicKeyframedTrack",
+                                "__debugName": "Heading 1:[\"rotateA\"]",
+                                "keyframes": [
+                                    {
+                                        "id": "Yz7DKJgjWj",
+                                        "position": 0,
+                                        "connectedRight": true,
+                                        "handles": [
+                                            0.5,
+                                            1,
+                                            0.5,
+                                            0
+                                        ],
+                                        "type": "bezier",
+                                        "value": 1.5509128289873653
+                                    }
+                                ]
                             }
                         },
                         "trackIdByPropPath": {
-                            "[\"y\"]": "KBWh-Kw46t"
+                            "[\"y\"]": "KBWh-Kw46t",
+                            "[\"boxX\"]": "LdTv2JyGT_",
+                            "[\"rotateA\"]": "9YTvAO-ziA"
                         }
                     }
                 }
@@ -68,6 +150,7 @@ const projectState = {
     },
     "definitionVersion": "0.4.0",
     "revisionHistory": [
+        "3dERApalXWBoXEDR",
         "RmwdiZ0lq3VVjGlK"
     ]
 }
